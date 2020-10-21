bf40580cf4149554a67d55f9b79d9be989516a0c
Timothy Pressey <pressey@whitewhaleanalytics.com>
2020-10-21 12:39:15 -0600

    change frontend to use functional components, remove response formatter from backend

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,10 @@
 'use strict';
-'use strict';
 const appPackage = require(__dirname + '/package.json');
 const Hapi = require('hapi');
 const colors = require('colors/safe');
 const Config = require('config');
 const utils = require('./services/utils/utils.js');
 const db = require('./services/utils/db.js');
-const responsePreCheck = require('./services/utils/formatter').responsePreCheck;
-
-
 
 async function start() {
 
@@ -19,33 +15,19 @@ async function start() {
 
         const server = new Hapi.Server(Config.util.toObject(Config.get('server.connection')));
 
-        /**
-         * Handles the options request and allows views
-         * and swagger through
-         */
-        server.ext('onPreResponse', function (req, h){
-
-            // Let the swagger stuff go through
-            if(req.response.variety === 'plain' && req.path !== '/swagger.json'
-                || req.response.isBoom){
-                return responsePreCheck(req, h);
-            }
-            return h.continue;
-        });
-
         await utils.addPolicies(server);
 
         utils.addRoute(server);
 
-        await server.start()
+        await server.start();
         console.log(colors.green('%s %s started on %s'), appPackage.name, appPackage.version, server.info.uri);
 
         module.exports = server;
 
     } catch (err) {
-        console.log(err)
-        process.exit(0)
+        console.log(err);
+        process.exit(0);
     }
 }
 
-start()
+start();
